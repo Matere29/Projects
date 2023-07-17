051507c4a51306050fa0586e4cef839ef918c17f
Rorisang Makgaka <125877628+Matere29@users.noreply.github.com>
2023-07-17 16:25:06 +0200

    Add files via upload

--- ToDoApp2/script.js
+++ ToDoApp2/script.js
@@ -23,13 +23,46 @@ function addItem(){
     setItems(items);
     refreshList();
 }
+function updateItem(item, key, value){
+    item[key] = value;
+
+    setItems(items);
+    refreshList();
+}
 function refreshList(){
     //sort items
+    items.sort((a,b) => {
+        if(a.completed) {
+            return 1;
+        }
+        if (b.completed) {
+            return -1   
+        }
+        return a.description < b.description ? -1 : 1;
+    })
 
     ITEMS_CONTAINER.innerHTML="";
     for(const item of items){
         const itemElement = ITEM_TEMPLATE.content.cloneNode(true);
         const descriptionInput = itemElement.querySelector(".item-description");
+        const completedInput = itemElement.querySelector(".item-completed");
+
+        description.value = item.description;
+        completedInput.checked = item.completed;
+        
+        descriptionInput.addEventListener("change", ()=>{
+            updateItem(item, "description", descriptionInput.value);
+        })
+        completedInput.addEventListener("change", ()=>{
+            updateItem(item, "completed", completedInput.checked);
+        })
+
+        ITEMS_CONTAINER.append(itemElement);
+
     }
 }
+ADD_BUTTON.addEventListener("click", () => {
+    addItem(item);
+});
+refreshList();
 /**time:14:31 */
